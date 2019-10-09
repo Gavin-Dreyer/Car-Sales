9e3fafe056dade03d25bda8f793c215da75a38cb
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-09 12:46:30 -0700

    Rendering state info with redux flow

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
+import { myReducer } from './reducers'
+
+import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+
+const store = createStore(myReducer)
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+     rootElement);
